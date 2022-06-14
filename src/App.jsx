56d933d83b337ec17af62826bf54ee8ce293aa5e
Wilmer Pocaterra <malgein17@gmail.com>
2022-06-13 23:51:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Index from './pages/index'
+import Create from './pages/create'
+import View from './pages/view'
+import './App.css';
+import Store from './store/store'
+
+
+function App() {
+  return (
+    <Store>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='create' element={<Create />} />
+          <Route path='view/:bookId' element={<View />} />
+        </Routes>
+      </BrowserRouter>
+    </Store>
+  );
+}
+
+export default App;
